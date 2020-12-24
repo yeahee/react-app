cfa9a49224ac75bf2253739c03fbcc457c3b9e11
Yehee <dpdml40234023@gmail.com>
2020-12-24 13:42:40 +0900

    Initialize react-app

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+/*public/index.html의 id="root"에서 index.js 파일 호출,
+  index.js 파일이 가장 먼저 실행, render 부분에서 App.js 호출*/
+ReactDOM.render(
+  <React.StrictMode>
+    <App /> 
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
