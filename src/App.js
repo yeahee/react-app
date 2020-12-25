36e4010e009d19b8a7b9366d5ee953231581ad9d
Yehee <dpdml40234023@gmail.com>
2020-12-25 18:04:34 +0900

    Create component events

--- src/App.js
+++ src/App.js
@@ -8,8 +8,12 @@ class App extends Component{
   /*render보다 먼저 실행되면서 컴포넌트 초기화 하고 싶으면 constructor에서 구현*/
   constructor(props){
     super(props);
+    /*state 값이 변경되면 render 다시 실행됨*/
     this.state ={
+      mode: 'read',
+      selected_content_id: 2,
       subject: {title: 'WEB', sub: 'World Wide Web!'},
+      welcome: {title: 'Welcome', desc: 'Hello, React!!'},
       contents:[
         {id: 1, title: 'HTML', desc: 'HTML is HyperText Markup Language.'},
         {id: 2, title: 'CSS', desc: 'CSS is for design.'},
@@ -19,14 +23,41 @@ class App extends Component{
   }
 
   render(){
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }
+    else if(this.state.mode === 'read'){
+      for(var i=0; i<3; i++){
+        if(this.state.selected_content_id === this.state.contents[i].id){
+          _title = this.state.contents[i].title;
+          _desc = this.state.contents[i].desc;
+          break;
+        }
+      } 
+    }
+
     return (
       <div className="App">
         <Subject
           title={this.state.subject.title}
-          sub={this.state.subject.sub}>
+          sub={this.state.subject.sub}
+          onChangePage={function(){
+            this.setState({mode: 'welcome'});
+          }.bind(this)}>
         </Subject>
-        <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <TOC 
+          data={this.state.contents}
+          onChangePage={function(id){
+            this.setState({
+              mode: 'read',
+              selected_content_id: Number(id)
+            });
+          }.bind(this)}
+        >
+        </TOC>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -5,8 +5,29 @@ class TOC extends Component{
         var lists = [];
         var data = this.props.data;
         for(var i=0; i<data.length; i++){
-            lists.push(<li key={data[i].id}><a href={"/content/" + data[i].id}>{data[i].title}</a></li>);
-        }
+            lists.push(
+                <li key={data[i].id}>
+                    <a href={"/content/" + data[i].id}
+                    data-id={data[i].id}
+                    onClick={function(e){
+                        e.preventDefault();
+                        this.props.onChangePage(e.target.dataset.id);
+                    }.bind(this)}>
+                        {data[i].title}
+                    </a>
+                </li>//e.target은 이벤트 발생 시 선택된 태그 가리킴, 'data-'로 시작하는 정보는 dataset에 있음
+
+            //     <li key={data[i].id}>
+            //     <a href={"/content/" + data[i].id}
+            //     onClick={function(id, e){
+            //         e.preventDefault();
+            //         this.props.onChangePage(id);
+            //     }.bind(this, data[i].id)}>
+            //         {data[i].title}
+            //     </a>
+            //     </li>
+            );
+        } 
         return(
             <nav>
                 <ul>
